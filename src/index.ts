3ca3e0fa858e818b641ac0a83f27ef0aaa45ee10
letsG <42511280+letsG@users.noreply.github.com>
2024-05-09 19:57:00 +0300

    feat: merge

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
-import { Telegraf } from 'telegraf';
+import { Telegraf, session, type Context } from 'telegraf';
 
 import { about } from './commands';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { development, production } from './core';
+import type { Update } from 'telegraf/types';
 
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
 
-const bot = new Telegraf(BOT_TOKEN);
+export interface SessionContext<U extends Update = Update> extends Context<U> {
+  session: {
+    shm: {
+      token: string;
+      user_id: string;
+    };
+  };
+}
+
+const bot = new Telegraf<SessionContext>(BOT_TOKEN);
+
+bot.use(
+  session({
+    defaultSession: () => ({
+      shm: {
+        token: '',
+        user_id: '',
+      },
+    }),
+  }),
+);
 
 bot.command('start', about());
 
